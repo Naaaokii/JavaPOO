bb1d7ace4e2ff242a8fd7d4b3f5f05f4d9470c1e
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-09 15:35:01 +0100

    accesseurs

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -2,17 +2,42 @@ package model;
 import java.util.Date;
 
 public class Contact {
-    private String _nom;
-    private String _prenom;
-    private String _mail;
-    private String _telephone;
-    private Date _dateNaissance;
-
-    public String getNom(){
-        return this._nom;
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String telephone;
+    private Date dateNaissance;
+    
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
     }
 
-    public void setNom(String valeur){
-        this._nom = valeur; 
-    }
+    
 }
