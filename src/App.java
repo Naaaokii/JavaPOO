3a56030bb077072bb447820a71a4da88eb3cbda2
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-09 15:14:33 +0100

    depart

---
+++ src/App.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class App {
+
+    private static Scanner _scan = new Scanner(System.in);
+        
+    public static void main(String[] args) {
+        afficherMenu(); 
+        while(true){
+            String choix = _scan.nextLine();
+            switch(choix){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "q":
+                    return;
+                default:
+                    System.out.println("NOPE !!!!");
+                    break;
+            }
+        }
+    }
+
+    private static void ajouterContact(){
+
+    }
+
+    public static void afficherMenu(){
+        ArrayList<String> menus = new ArrayList<>();
+        menus.add(("-- MENU --"));
+        menus.add(("1- Ajouter un contact"));
+        menus.add(("2- Lister les contacts"));
+        menus.add(("q- Quitter"));
+        for (String menu : menus){
+            System.out.println(menu);
+        }
+    }
+}
---
+++ src/model/Contact.java
@@ -0,0 +1,11 @@
+package model;
+import java.util.Date;
+
+public class Contact {
+    private String _nom;
+    private String _prenom;
+    private String _mail;
+    private String _telephone;
+    private Date _dateNaissance;
+
+}
